00fb31c452bd111de6a3dcd8ff9421e60d3abb43
rakosg <rakog@oregonstate.edu>
2021-09-30 23:00:06 -0700

    completed c01

--- cploration/c01/main.c
+++ cploration/c01/main.c
@@ -3,25 +3,54 @@
 /* Ex.1: Return the larger value */
 int max(int x, int y) 
 {
-   // YOUR CODE HERE
+   if (x > y){
+      return x;
+   }
+      
+   else {
+      return y;
+   }
+      
+      
 }
 
 /* Ex.2: Add [0..100] and return the sum */
 int add100()
 {
-   // YOUR CODE HERE
+   int sum = 0;
+
+   for (int i = 0; i < 100; i++) {
+      sum += i;
+   }
+   
+   return sum;
 }  
 
-/ * Ex.3: Sum the elements in the array */
+/* Ex.3: Sum the elements in the array */
 int sum(int arr[], int n)
 {
-   // YOUR CODE HERE
+   int sum_of_elements = 0;
+
+   for (int i = 0; i < n; i++) {
+      sum_of_elements += arr[i];
+   }
+
+   return sum_of_elements;
 }  
 
-/ * Ex.4: Find the largest element in the array */
+/* Ex.4: Find the largest element in the array */
 int largest(int arr[], int n)
 {
-   // YOUR CODE HERE    
+   int x;
+
+   x = arr[0];
+
+   for (int i = 1; i < n; i++) {
+      if (arr[i] > x) {
+         x = arr[i];
+      }
+   }
+   return x;
 }
 
 /* Provided */
